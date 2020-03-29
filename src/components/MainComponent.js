13320de6e47123e23bdde870327759c53882c4f2
LadyDeveloper <anhacorrea@hotmail.com>
2020-03-29 10:26:30 -0700

    Week 3 Workshop Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+import About from './AboutComponent';
 class Main extends Component {
   constructor(props){
     super(props);
@@ -31,13 +32,24 @@ class Main extends Component {
               />
           )
       }
+
+      const CampsiteWithId = ({match}) => {
+          return(
+            <CampsiteInfo
+                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+            />
+          );
+      }
       return (
           <div>
               <Header />
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                 <Route exact path='/contactus' component={Contact} />
+                <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                 <Redirect to='/home' />
                 {/* <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} /> */}
             </Switch>  
