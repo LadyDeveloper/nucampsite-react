6558acbf9c4535deb2244ec4aecc8b1998c94716
LadyDeveloper <anhacorrea@hotmail.com>
2020-04-04 01:08:13 -0700

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
+import { configureStore } from './redux/configureStore';
 import './App.css';
 
-const store = ConfigureStore();
+const store = configureStore();
 
 class App extends Component {
   render() {
