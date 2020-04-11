4a6e8d1985115c511953951d6e24ca73207e9999
LadyDeveloper <anhacorrea@hotmail.com>
2020-04-10 23:27:15 -0700

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
 function RenderCard( { item, isLoading, errMess }){
     const returnName = console.log('Item is ' + {item});
@@ -16,38 +17,42 @@ function RenderCard( { item, isLoading, errMess }){
         );
     }
     return(
-        <Card>
-            {/* <CardImg src={baseUrl + item.image} alt={item.name} /> */}
-            <CardBody>
-                <CardTitle>{item.name} {returnName}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+            in 
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(50%)'
+            }}>
+            <Card>
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name} {returnName}</CardTitle>
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
 function Home(props){
     return(
         <div className="container">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md m-1">
-                        <RenderCard 
-                            item={props.campsite} 
-                            isLoading={props.campsitesLoading}
-                            errMess={props.campsitesErrMess}
-                        />
-                    </div>
-                    <div className="col-md m-1">
-                        <RenderCard 
-                            item={props.promotion}
-                            isLoading={props.promotionLoading}
-                            errMess={props.promotionErrMess}
-                        />
-                    </div>
-                    <div className="col-md m-1">
-                        <RenderCard item={props.partner} />
-                    </div>
+            <div className="row">
+                <div className="col-md m-1">
+                    <RenderCard 
+                        item={props.campsite} 
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
+                    />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard 
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner} />
                 </div>
             </div>
         </div>
